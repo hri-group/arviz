83456aa16eca3855b08e1863d759d71e69b1f4b8
StevenHoang <khoahoang08091997@hotmail.com>
2021-01-19 12:12:20 +1100

    Preliminary ArrowTool added ArrowTool along with simple (yet modifiable) Grid Display

---
+++ Assets/Scripts/DefaultPlugins/GridDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Implement GridDisplay to be the same as RViz
+public class GridDisplay : MonoBehaviour
+{
+    private float gridThickness = 0.001f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetPlaneCellCount()
+    {
+        
+    }
+    public void SetNormalCellCount()
+    {
+
+    }
+}
