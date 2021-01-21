b4fe2c02b58da19dae72af1c392280b688163c03
Steven Hoang <khoahoang08091997@hotmail.com>
2021-01-21 19:30:08 +1100

    2D Nav tool implemented with hardcoded orientation Wait for better implemetation for orientation interpretation

--- Assets/Scripts/Tools/NavigationTool.cs
+++ Assets/Scripts/Tools/NavigationTool.cs
@@ -14,6 +14,8 @@ public class NavigationTool : MonoBehaviour, IMixedRealityPointerHandler
     const int ORIENTATION_STATE = 1;
     private int state = 0;
     private GameObject arrow_clone;
+    [SerializeField]
+    private GameObject referenceFrame;
     public void OnPointerClicked(MixedRealityPointerEventData eventData)
     {
         switch (state)
@@ -26,7 +28,6 @@ public class NavigationTool : MonoBehaviour, IMixedRealityPointerHandler
                     arrow_clone.GetComponent<ArrowManipulation>().SetArrow(Vector3.zero, Vector3.forward);
                     arrow_clone.transform.parent = transform.parent; // Parent of GridDisplay aka ImageTarget
                     arrow_clone.transform.position = result.Details.Point;
-                    arrow_clone.transform.localPosition = new Vector3(arrow_clone.transform.localPosition.x, 0, arrow_clone.transform.localPosition.z);
                     arrow_clone.GetComponent<ArrowManipulation>().SetFollowCursor(true);
                 }
                 state = ORIENTATION_STATE;
@@ -35,9 +36,16 @@ public class NavigationTool : MonoBehaviour, IMixedRealityPointerHandler
                 RosSharp.RosBridgeClient.MessageTypes.Geometry.PoseStamped goal = new RosSharp.RosBridgeClient.MessageTypes.Geometry.PoseStamped();
                 goal.header.Update();
                 goal.header.frame_id = "map"; //Hardcoded as map for now, change later
-                goal.pose = new RosSharp.RosBridgeClient.MessageTypes.Geometry.Pose(
-                                        transform.InverseTransformPoint(arrow_clone.transform.position).Unity2Ros().unity2RosPointMsg(),
-                                        arrow_clone.transform.rotation.Unity2Ros().unity2RosQuaternionMsg());
+                Debug.Log(referenceFrame.transform.InverseTransformPoint(arrow_clone.transform.position).Unity2Ros());
+                Debug.Log(arrow_clone.transform.rotation.Unity2Ros().unity2RosQuaternionMsg());
+                RosSharp.RosBridgeClient.MessageTypes.Geometry.Point position = referenceFrame.transform.InverseTransformPoint(arrow_clone.transform.position).Unity2Ros().unity2RosPointMsg();
+                position.z = 0;
+                RosSharp.RosBridgeClient.MessageTypes.Geometry.Quaternion orientation = (arrow_clone.transform.rotation*Quaternion.Inverse(GameObject.Find("ImageTarget").transform.localRotation)).Unity2Ros().unity2RosQuaternionMsg();
+                orientation.x = 0;
+                orientation.y = 0;
+                orientation.z = 0;
+                orientation.w = 1;
+                goal.pose = new RosSharp.RosBridgeClient.MessageTypes.Geometry.Pose(position, orientation);
                 GameObject.Find("/ROS Connector").GetComponent<NavigationPublisher>().SendGoal(goal);
                 Destroy(arrow_clone);
                 state = POSITION_STATE;
