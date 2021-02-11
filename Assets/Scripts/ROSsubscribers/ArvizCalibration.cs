87e688e7e32878c3253ad7bd3243c48468f7cbf7
StevenHoang <khoahoang08091997@hotmail.com>
2021-02-12 01:04:54 +1100

    ARviz Display Calibration with ROS publisher Publish a TransformStamped message to /arviz_calibration with the transform from the QR code to the root of the TF tree

---
+++ Assets/Scripts/ROSsubscribers/ArvizCalibration.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RosSharp;
+using RosSharp.RosBridgeClient;
+using RosSharp.RosBridgeClient.MessageTypes.Tf2;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class ArvizCalibration : UnitySubscriber<MessageTypes.Geometry.TransformStamped>
+    {
+        private bool isMessageReceived = false;
+        private UnityEngine.Vector3 PublishedPosition;
+        private UnityEngine.Quaternion PublishedOrientation;
+        private string childFrame;
+        public GameObject TF;
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            PublishedOrientation = new Quaternion();
+            PublishedPosition = new Vector3();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isMessageReceived)
+            {
+                ProcessMessage();
+            }
+        }
+        protected override void ReceiveMessage(MessageTypes.Geometry.TransformStamped message)
+        {
+            childFrame = message.child_frame_id;
+            PublishedPosition = message.transform.translation.rosMsg2Unity().Ros2Unity();
+            PublishedOrientation = message.transform.rotation.rosMsg2Unity().Ros2Unity();
+            isMessageReceived = true;
+        }
+        void ProcessMessage()
+        {
+            if(TF != null)
+            {
+                TF.transform.localPosition = PublishedPosition;
+                TF.transform.localRotation = PublishedOrientation;
+                isMessageReceived = false;
+            }
+        }
+    }
+}
