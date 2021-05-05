63499ceb807935b410ff24a7490c76a048f55d2b
StevenHoang <khoahoang08091997@hotmail.com>
2021-05-06 02:10:49 +1000

    Added tag for TFArrow and modified hide/unhide method for TF

--- Assets/Scripts/DefaultPlugins/TFButtonReceiver.cs
+++ Assets/Scripts/DefaultPlugins/TFButtonReceiver.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 using UnityEngine.Events;
 using Microsoft.MixedReality.Toolkit.UI;
 using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections.Generic;
+using System.Linq;
 
 public class TFButtonReceiver : ReceiverBase
 {
@@ -27,37 +29,49 @@ public class TFButtonReceiver : ReceiverBase
 
     public override void OnClick(InteractableStates state, Interactable source, IMixedRealityPointer pointer = null)
     {
-        GameObject[] TFobj = GameObject.FindGameObjectsWithTag("TF");
+        List<GameObject> tfAxesAndName = GameObject.FindGameObjectsWithTag("TF").ToList();
+        List<GameObject> tfArrow = GameObject.FindGameObjectsWithTag("TFArrow").ToList();
 
         base.OnClick(state, source);
 
         if (source.transform.name == "ShowNameCheckbox")
         {
-            foreach (var tf in TFobj)
+            foreach (var tf in tfAxesAndName)
             {
-                if (tf.transform.GetChild(1).localScale.x != 0)
-                    tf.transform.GetChild(1).localScale = new Vector3(0, 0, 0);
+                if (tf.transform.GetChild(1).gameObject.activeInHierarchy)
+                    tf.transform.GetChild(1).gameObject.SetActive(false);
                 else
-                    tf.transform.GetChild(1).localScale = new Vector3(1, 1, 1);
-            }   
+                    tf.transform.GetChild(1).gameObject.SetActive(true);
+            }
         } 
         else if (source.transform.name == "ShowTFCheckbox")
         {
-            foreach (var tf in TFobj)
+            foreach (var tf in tfAxesAndName)
             {
-                if (tf.transform.GetChild(0).localScale.x != 0)
-                    tf.transform.GetChild(0).localScale = new Vector3(0, 0, 0);
+                if (tf.transform.GetChild(0).gameObject.activeInHierarchy)
+                    tf.transform.GetChild(0).gameObject.SetActive(false);
                 else
-                    tf.transform.GetChild(0).localScale = new Vector3(1, 1, 1);
+                    tf.transform.GetChild(0).gameObject.SetActive(true);
             }
-        } 
+        }
+        else if (source.transform.name == "ShowArrowCheckbox")
+        {
+            foreach (var tf in tfArrow)
+            {
+                if (tf.activeInHierarchy)
+                    tf.SetActive(false);
+                else
+                    tf.SetActive(true);
+            }
+        }
         else
         {
             string tf_name = source.transform.name.Remove(source.transform.name.Length - 9);
-            GameObject target_tf = GameObject.Find(tf_name);
-
+            GameObject target_tf = tfAxesAndName.Find(res => res.name == tf_name + "_tf");
+            GameObject target_arrow = tfArrow.Find(res => res.name == tf_name + "_arrow");
             target_tf.transform.GetChild(1).gameObject.SetActive(!target_tf.transform.GetChild(1).gameObject.activeInHierarchy);
             target_tf.transform.GetChild(0).gameObject.SetActive(!target_tf.transform.GetChild(0).gameObject.activeInHierarchy);
+            target_arrow.SetActive(!target_arrow.activeInHierarchy);
         }
     }
 }
